7135685d229869f2f4aabc293df9f42c4f2964d8
CiCiCat <379845866@qq.com>
2018-12-13 19:43:28 +0800

    Initial commit

---
+++ src/tools/DataBaseUtil.java
@@ -0,0 +1,44 @@
+package tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by CWZZ on 2016/11/25.
+ */
+public class DataBaseUtil {
+
+    /**
+     * 获取数据库连接
+     *
+     * @return Connection 对象
+     */
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/bwl";
+            connection = DriverManager.getConnection(url, "root", "zxcvbn");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return connection;
+    }
+
+    /**
+     * 关闭数据库连接
+     */
+    public static void closeConnection(Connection connection) {
+        //判断connection是否为空
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+
+}
